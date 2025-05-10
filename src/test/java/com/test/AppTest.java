7f77247a1793684c07a3f8da65ecd65191c2225f
root <root@ip-172-31-2-200.ap-south-1.compute.internal>
2025-05-10 11:01:42 +0000

    first

---
+++ src/test/java/com/test/AppTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testSomething() {
+        assertTrue(true);
+    }
+}
+
